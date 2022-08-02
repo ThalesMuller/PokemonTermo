6300253509c8552e61b5c77a43f54a087da97fd7
Thales Eduardo Muller <thales.muller@compasso.com.br>
2022-08-02 15:27:15 -0300

    feat: apply new theme

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import themes from "./utils/themes";
+import themes, { newTheme } from "./utils/themes";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./global.styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={themes.default}>
+        <ThemeProvider theme={newTheme}>
             <GlobalStyle />
             <App />
         </ThemeProvider>
